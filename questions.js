b9183c9421477fc342c6ffdd86004a151ff73e87
James Dobbe <dobbe2@gmail.com>
2020-01-05 23:44:54 -0800

    stiill debugging the timer and certain elemts being set to null in the console. I did get some CSS properties added to my buttons. having a lot of difficulties at this current spot.

--- questions.js
+++ questions.js
@@ -1,27 +1,27 @@
 let questions = [
     {
-        title: "Question 1:",
-        choices: ["answer1", "answer2", "answer3", "answer4"],
+        title: "What tag is not a self closing tag?",
+        choices: ["&ltimg&gt image tag", "&ltlink&gt link tag", "&lta&gt anchor tag", "&ltsource&gt source tag"],
         answer: 2
     },
     {
-    title: "Question 2:",
-    choices: ["answer1", "answer2", "answer3", "answer4"],
+    title: "How would you create a new &ltdiv&gt element using the Javascript and the DOM?",
+    choices: ["You must creat div inside the HTML", "add.new.div.to.html", "document.querySelector('div').appendChild", "document.createElement('div')"],
     answer: 3
     },
     {
-        title: "Question 3:",
-        choices: ["answer1", "answer2", "answer3", "answer4"],
+        title: "What is the correct HTML for inserting an image?",
+        choices: ["&ltimg alt='MyImage'&gtimage.jpeg&lt/img&gt", "&ltimg src='image.jpeg' alt='MyImage'&gt", "&ltimg href='image.jpeg' alt='MyImage'&gt", "&ltimage src='image.jpeg' alt='MyImage'&gt"],
         answer: 1
     },
     {
-        title: "Question 4:",
-        choices: ["answer1", "answer2", "answer3", "answer4"],
+        title: "What is the correct CSS syntax?",
+        choices: ["h1 {color: red;}", "{h1:color= red;}", "{h1;color:red;}", "h1: color= red;"],
         answer: 0
     },
     {    
-        title: "Question 5:",
-    choices: ["answer1", "answer2", "answer3", "answer4"],
+        title: "Which of these statements is true?",
+    choices: ["'five' == 5", "5 == '5'", "5 === '5'", "'five' === '5'"],
     answer: 1
 },
 ];
--- script.js
+++ script.js
@@ -4,32 +4,38 @@ let startButton = document.querySelector("#start-quiz");
 let currentQuestion = 0;
 let correctAnswer = 0;
 let incorrectAnswer = 0;
-
-//starting the quiz
+let score = 0;
+let myInterval = 0;
+//starting the quiz, setting the timer
 startButton.addEventListener("click", function(){
     event.preventDefault();
-    showQuestions()
-    let interval = setInterval(function(){
+    showQuestions();
+    let myInterval = setInterval(function(){
         // decrement the timer 
-        timer--
+        timer--;
         // show the timer on the DOM
-        document.querySelector("#timer").textContent = timer
+        if (timer > 0) 
+        document.querySelector("#timer").textContent = "Time: " + timer + " seconds remaining"
         // verify if timer < 0 then if yes finish
         if (timer <= 0){
-            clearInterval(timer)
+            clearInterval(myInterval);
+            // document.querySelector("#timer").textContent = ""
             //show result form
             results()
+           
         }
-        
     }, 1000);
+
     
 })
 
+//show the questions for the quiz
 function showQuestions(){
     document.querySelector("#starting-quiz").innerHTML = "";
     let current = questions[currentQuestion]
     document.querySelector("#question").innerHTML = current.title
     document.querySelector("#response").innerHTML = ""
+    document.querySelector("#correct").innerHTML = ""
     for(var i = 0; i < current.choices.length; i ++){
         let choice = document.createElement("button")
         choice.innerHTML = current.choices[i]
@@ -38,33 +44,54 @@ function showQuestions(){
         choice.onclick = verify
         document.querySelector("#response").appendChild(choice)
     }
-
 }
+
 function verify(){
     console.log(this.value, questions[currentQuestion].answer)
     if(questions[currentQuestion].answer === parseInt(this.value)){
         // correct
         correctAnswer++
+        score = score + 10
+        let timeout = setTimeout(function(){
+           document.querySelector("#correct").innerHTML = "Correct!"
+
+        }, 100);
+        
+        
     }else{
         //incorrect
         incorrectAnswer++
         timer = timer - 15
     }
     console.log(correctAnswer, incorrectAnswer);
+
+// verify if currentquestion was the last then finish or if not increment currentquestion and go to show the next
+
         currentQuestion++
         if(currentQuestion === questions.length){
             //show Results, the end
+            score = score + timer
+           
             results ()
-        }else{
+            clearInterval(myInterval)
+        }
+        else{
             showQuestions()
         }
-    
-    // verify if currentquestion was the last then finish or if not increment currentquestion and go to show the next
 }
 
 function results(){
-    document.querySelector("#quiz-area").innerHTML = ""
-    document.querySelector("#timer").innerHTML = ""
-    document.querySelector("h3").innerHTML = "Final Score"
+    document.querySelector("h3").innerHTML = "Final Score";
+    document.querySelector("#quiz-area").innerHTML = score;
+    // document.querySelector("#highscore").textContent = score;
+    // document.querySelector("#highscore")
+    playerInitials = prompt("What is your initials?");
+    console.log(playerInitials)
 }
 //show results
+//save score into local storage
+function saveScore(){
+    window.localStorage.setItem(playerInitials, score);
+    console.log(playerInitials, score)
+}
+
